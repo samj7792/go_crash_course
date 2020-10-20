f8b40e8597d159f090abb2addbb5b7c636ae3209
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-20 11:35:34 -0400

    packages and functions

---
+++ 03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/samj7792/go_crash_course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutil.Reverse("olleH"))
+}
---
+++ 04_functions/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(greeting("Sam"))
+	fmt.Println(getSum(1, 2))
+}
